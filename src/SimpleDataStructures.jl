2d3619485484eed0b3d30348093200d90bb4dda5
harryscholes <harryscholes@gmail.com>
2020-05-22 14:10:13 +0100

    Export SimpleDict and SimpleDefaultDict

--- src/SimpleDataStructures.jl
+++ src/SimpleDataStructures.jl
@@ -3,12 +3,16 @@ module SimpleDataStructures
 export
     SimpleVector,
     SimpleSparseVector,
+    SimpleDict,
+    SimpleDefaultDict,
     SimpleLinkedList,
     SimpleStack,
     SimpleQueue
 
 include("vector.jl")
 include("sparse_vector.jl")
+include("dict.jl")
+include("default_dict.jl")
 include("doubly_linked_list.jl")
 include("stack.jl")
 include("queue.jl")
